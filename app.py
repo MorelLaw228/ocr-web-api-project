2b419b9d15a1b733b5b7ee8b64b4499cc33ac0ef
“MorelLaw228” <morellatel@gmail.com>
2021-09-18 13:19:16 +0200

    First commit

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def respond():
         response["ERROR"] = "name can't be numeric."
     # Now the user entered a valid name
     else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+        response["MESSAGE"] = f"Bienvenue  {name}  sur notre merveilleuse plateforme !!"
 
     # Return the response in json format
     return jsonify(response)
@@ -33,7 +33,7 @@ def post_something():
     # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
     if param:
         return jsonify({
-            "Message": f"Welcome {param} to our awesome platform!!",
+            "Message": f"Bienvenue à toi  {param} sur notre merveilleuse plateforme !!",
             # Add this option to distinct the POST request
             "METHOD": "POST"
         })
